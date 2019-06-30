22e9ddff9b1fc2d3d9dffafd3755250ad4c812bd
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2019-06-30 14:45:31 -0500

    First commit

---
+++ contactValidation.js
@@ -0,0 +1,7 @@
+function validateContact(){
+    var contactName = document.getElementById("contact-name");
+    var contactEmail = document.getElementById("email");
+    var contactNumber = document.getElementById("number");
+    var contactReason = document.getElementById("reason");
+    
+}
