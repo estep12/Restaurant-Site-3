7abac246b3f39d6bde9ed0a4b7479d22b70fba3f
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2019-06-30 15:58:41 -0500

    added pics to carousel. added contact validation

--- contactValidation.js
+++ contactValidation.js
@@ -1,7 +1,16 @@
-function validateContact(){
-    var contactName = document.getElementById("contact-name");
-    var contactEmail = document.getElementById("email");
-    var contactNumber = document.getElementById("number");
-    var contactReason = document.getElementById("reason");
-    
+function validateContact() {
+    var contactName = document.getElementById("contact-name").value;
+    var contactEmail = document.getElementById("email").value;
+    var contactNumber = document.getElementById("number").value;
+    var contactReason = document.getElementById("reason").value;
+    var checkbox = document.querySelectorAll("input.checkbox:checked");
+    var validNum = /^\d{10}$/.test(contactNumber);
+
+
+
+    if (contactName == " " || contactEmail == " " || contactNumber == " " || checkbox.lenght === 0 || validNum === false) {
+        alert("Please fill in required information.")
+    } else {
+        alert("Thank you for contacting  us. We will be in touch shortly")
+    }
 }
